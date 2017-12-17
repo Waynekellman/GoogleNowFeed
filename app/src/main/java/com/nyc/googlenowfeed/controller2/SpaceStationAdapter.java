035d3b58b497e975bfce7f7e15a325f9f0cef73b
Darnell O <DarnellOtto@ac.c4q.nyc>
2017-12-17 17:54:59 -0500

    first commit

---
+++ app/src/main/java/com/nyc/googlenowfeed/controller2/SpaceStationAdapter.java
@@ -0,0 +1,8 @@
+package com.nyc.googlenowfeed.controller2;
+
+/**
+ * Created by D on 12/16/17.
+ */
+
+public class SpaceStationAdapter {
+}
---
+++ app/src/main/java/com/nyc/googlenowfeed/models2/Iss_Position.java
@@ -0,0 +1,8 @@
+package com.nyc.googlenowfeed.models2;
+
+/**
+ * Created by D on 12/16/17.
+ */
+
+public class Iss_Position {
+}
